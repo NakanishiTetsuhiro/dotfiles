eddd4f9eba8bcbd73183a5015e640571abaee804
NakanishiTetsuhiro <kaoru6strings@gmail.com>
2016-06-10 15:22:56 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,13 +4,6 @@
 DOTPATH=~/dotfiles
 
 
-if [ "$#" -ne 1 -o ! -r "$1" ]; then
-
-    echo "usage: ./install.sh -d"
-    exit 1
-fi
-
-
 deploy() {
 
     cd $DOTPATH
@@ -27,40 +20,5 @@ deploy() {
     done
 }
 
-
-initialize() {
-    
-    # git が使えるなら git
-    if hash "git"; then
-        git clone --recursive "$GITHUB_URL" "$DOTPATH"
-    
-    # 使えない場合は curl か wget を使用する
-    elif hash "curl" || has "wget"; then
-        tarball="https://github.com/NakanishiTetsuhiro/dotfiles/archive/master.tar.gz"
-    
-        # どっちかでダウンロードして，tar に流す
-        if hash "curl"; then
-            curl -L "$tarball"
-    
-        elif hash "wget"; then
-            wget -O - "$tarball"
-    
-        fi | tar xv -
-    
-        # 解凍したら，DOTPATH に置く
-        mv -f dotfiles-master "$DOTPATH"
-    
-    else
-        die "curl or wget required"
-    fi
-    
-}
-
-
-if [ "$1" = "deploy" -o "$1" = "-d" ]; then
-    deploy
-elif [ "$1" = "init" -o "$1" = "-i" ] ; then
-    initialize
-    deploy
-fi
+deploy
 
