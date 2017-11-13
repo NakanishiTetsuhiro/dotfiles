e1991795602c04585c2cf0debf5ed6c25e9c48d8
NakanishiTetsuhiro <tnakanishi@calcinc.co.jp>
2017-11-13 14:21:33 +0900

    Add fishshell settings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,20 @@
 #-------------------------
+# oh-my-fish configs
+#-------------------------
+# Path to Oh My Fish install.
+set -q XDG_DATA_HOME
+  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
+  or set -gx OMF_PATH "$HOME/.local/share/omf"
+
+# Load Oh My Fish configuration.
+source $OMF_PATH/init.fish
+
+# set fish_theme agnoster
+function fish_user_key_bindings
+  bind \cr 'peco_select_history (commandline -b)'
+end
+
+#-------------------------
 # Basic
 #-------------------------
 set -x XDG_CONFIG_HOME $HOME/.config
