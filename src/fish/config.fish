0bacd9c26700bfdca99c32b69e1533048d975684
Nakanishi Tetsuhiro <kaoru6strings@gmail.com>
2018-02-17 09:35:37 +0900

    Fix infinite roop error

--- src/fish/config.fish
+++ src/fish/config.fish
@@ -6,9 +6,6 @@ set -q XDG_DATA_HOME
   and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
   or set -gx OMF_PATH "$HOME/.local/share/omf"
 
-# Load Oh My Fish configuration.
-source $OMF_PATH/init.fish
-
 # set fish_theme agnoster
 set fish_theme default
 
